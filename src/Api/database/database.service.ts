92055ab9d65acd7a4fbdfa0cc79bd875ce5c480b
r7 <r7@smtechnology.kr>
2024-06-19 11:29:14 +0900

    [FEAT] 데이터 베이스 연결 에러시 1분에 한번식 재시도 로직추가

--- src/Api/database/database.service.ts
+++ src/Api/database/database.service.ts
@@ -9,33 +9,7 @@ export default class DatabaseService {
   private db: sqlite3.Database;
 
   constructor() {
-    new DownloadService().downloadFile().finally(() => {
-      //상대경로로 하면 db파일을 dist로 옮기면서 내용이 유실됨
-      this.db = new sqlite3.Database('src/Public/skullking.db', (err) => {
-        if (err) {
-          console.error('Error opening database:', err);
-        } else {
-          console.log('Connected to the SQLite database.');
-        }
-      });
-    });
-  }
-
-  async addBinaryData(data: Buffer): Promise<number> {
-    return new Promise((resolve, reject) => {
-      // 데이터베이스에 바이너리 데이터 추가
-      this.db.run(
-        `INSERT INTO files (data) VALUES (?);`,
-        [data],
-        function (err) {
-          if (err) {
-            reject(err);
-          } else {
-            resolve(this.lastID); // 삽입된 데이터의 ID 반환
-          }
-        },
-      );
-    });
+    this.connected();
   }
 
   query(sql: string, params: any[]): Promise<any> {
@@ -50,15 +24,43 @@ export default class DatabaseService {
     });
   }
 
+  async connected() {
+    return await new DownloadService()
+      .downloadFile()
+      .catch(() => {
+        setTimeout(() => {
+          return this.connected();
+        }, 60000);
+      })
+      .finally(() => {
+        //상대경로로 하면 db파일을 dist로 옮기면서 내용이 유실됨
+        return (this.db = new sqlite3.Database(
+          'src/Public/skullking.db',
+          (err) => {
+            if (err) {
+              console.error('Error opening database:', err);
+              setTimeout(() => {
+                return this.connected();
+              }, 60000);
+            } else {
+              console.log('Connected to the SQLite database.');
+            }
+          },
+        ));
+      });
+  }
+
   close(): void {
     this.db.close((err) => {
       if (err) {
         console.error('Error closing database:', err);
       } else {
-        const filePath = path.join(__dirname, 'Public', 'skullking.db');
+        const filePath = path.join('src', 'Public', 'skullking.db');
         fs.unlinkSync(filePath);
         console.log('Disconnected from the SQLite database.');
       }
+
+      this.connected();
     });
   }
 }
